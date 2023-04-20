345492ac3f2596dc9b9040abb2c6365e96ff4b5f
Katycevallos <127429335+Katycevallos@users.noreply.github.com>
2023-04-20 11:55:00 -0500

    Add files via upload

---
+++ trivia.js
@@ -0,0 +1,40 @@
+
+function correcto(){
+	window.alert("Correcto 1 punto")
+		
+}
+function incorrecto(){
+    window.alert("Incorrecto 0 puntos")
+}
+
+function mostrarPopup() {
+	var emergente = document.getElementById("emergente");
+	var Inf = document.getElementById("Inf");
+	emergente.
+	p
+style.display = "block";
+	emergente.
+	
+style.display = "block";
+}
+
+function ocultarPopup() {
+	var emergente = document.getElementById("emergente");
+	var Inf = document.getElementById("Inf");
+	var nombre = document.getElementById("nombre");
+	
+	alert
+alert("Hola, " + nombre.value + "!:)");
+	emergente.style.display = "none";
+	Inf.
+	
+style.display = "none";
+}
+
+ 
+var answer = form.querySelectorAll(".correcta")
+
+function comprobar() {
+	answer.checked 
+}
+	
